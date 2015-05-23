d0f4bebe779778a6a040b1f18bc0090ef9ed2d36
Yousha Aleayoub <yousha.a@hotmail.com>
2015-05-23 10:36:11 +0330

    Update example with last changes.

--- example.php
+++ example.php
@@ -2,27 +2,52 @@
 
 require_once 'source/HttpClient.php';
 
-// GET using CURL driver.
-$client = new HttpClient('curl');
-$client->setHeaders(array('User-Agent' => 'MyCustomAgent/1.0'));
-$response = $client->get('https://yahoo.com');
-print_r($response);
+$options = array(
+   'timeout' => 10,
+   'default_headers' => array(
+      'User-Agent' => 'MyCustomAgent/2.0',
+      'Accept' => 'application/json'
+   )
+);
 
-// POST using Socket driver.
-$client = new HttpClient('socket');
-$response = $client->post('https://yahoo.com/', array('name' => 'Test'));
-print_r($response);
+try {
+   // GET using CURL driver
+   $client = new HttpClient('curl', $options);
+   $response = $client->get('https://yahoo.com ');
+   echo "GET Response (Status: {$response['status']}):\n";
+   print_r($response);
+   echo "\n";
 
-// PUT Request.
-$response = $client->put('https://api.yahoo.com/resource/1 ', array(
-   'name' => 'Updated Name'
-), array(
-   'Content-Type' => 'application/json'
-));
-print_r($response);
+   // POST using Socket driver
+   $client = new HttpClient('socket', $options);
+   $response = $client->post('https://yahoo.com ', array('name' => 'Test'));
+   echo "POST Response (Status: {$response['status']}):\n";
+   print_r($response);
+   echo "\n";
 
-// DELETE Request.
-$response = $client->delete('https://api.yahoo.com/resource/1 ', array(
-   'Authorization' => 'Bearer token123'
-));
-print_r($response);
+   // PUT Request
+   $response = $client->put('https://api.yahoo.com/resource/1 ', array(
+      'name' => 'Updated Name'
+   ), array(
+      'Content-Type' => 'application/json'
+   ));
+   echo "PUT Response (Status: {$response['status']}):\n";
+   print_r($response);
+   echo "\n";
+
+   // DELETE Request
+   $response = $client->delete('https://api.yahoo.com/resource/1 ', array(
+      'Authorization' => 'Bearer token123'
+   ));
+   echo "DELETE Response (Status: {$response['status']}):\n";
+   print_r($response);
+   echo "\n";
+} catch (HttpClientException $e) {
+   echo "HttpClientException caught:\n";
+   echo "Message: " . $e->getMessage() . "\n";
+   echo "Code: " . $e->getCode() . "\n";
+   echo "Context:\n";
+   print_r($e->getContext());
+   echo "\n";
+   echo "Full Trace:\n" . $e . "\n";
+}
